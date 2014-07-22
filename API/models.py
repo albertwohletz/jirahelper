9bd397ec1fa30d3464db60ef8b2c1ab1e7c5c3d2
Albert Wohletz <albert@wohletz.net>
2014-07-21 18:33:40 -0700

    Created database model.

--- API/models.py
+++ API/models.py
@@ -1,6 +1,20 @@
 __author__ = 'albertlwohletz'
 from django.db import models
 
+class Users(models.Model):
+    user_name = models.CharField(max_length=200)
+    email_address = models.CharField(max_length=200)
+
+class Spaces(models.Model):
+    name = models.CharField(max_length=200)
+
 class Access(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    user_id = models.ForeignKey('Users')
+    space_id = models.ForeignKey('Spaces')
+    access_type = models.CharField(max_length=200)
+
+class Pending(models.Model):
+    user_id = models.ForeignKey('Users')
+    space_id = models.ForeignKey('Spaces')
+    access_type = models.CharField(max_length=200)
+    explanation_string = models.CharField(max_length=200)
